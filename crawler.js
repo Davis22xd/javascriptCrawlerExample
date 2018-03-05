26cceab9a9aa897652914dc501c60de150fc06e8
david <davisxdpfr@gmail.com>
2018-03-04 21:29:35 -0500

    Created sort methods to compare items in array of objects

--- crawler.js
+++ crawler.js
@@ -23,6 +23,7 @@ request(pageToVisit, function(error, response, body) {
         setListFromAtributte($,'.rank','rank');
         setListFromAtributte($,'.storylink','title');
         setListFromAtributte($,'.score','score');
+        convertScoreInNumbers();
         setItemsIdInlistItems($,'.athing');
 
         var commentsList = getCommentsListIfContainWords($);
@@ -31,18 +32,45 @@ request(pageToVisit, function(error, response, body) {
         console.log("The 30th first items in the page are:")
         console.log(mainList);
 
+
         console.log("Entries with more than five words in the title ordered by amount of comments: ");
         moreThanFiveWordsList = getEntriesWithMoreThanFiveWordsInTitle();
+        moreThanFiveWordsList.sort(compareByNumberOfComments);
         console.log(moreThanFiveWordsList);
 
         console.log("Entries with less than or equal to five words in the title ordered by points: ");
         lessThanSixWordsList = getEntriesWithLessThanWordsInTitle();
+        lessThanSixWordsList.sort(compareByScore);
         console.log(lessThanSixWordsList);
-
 
     }
 });
 
+function compareByNumberOfComments(a,b) {
+    if (a.numberOfComments < b.numberOfComments)
+        return -1;
+    if (a.numberOfComments > b.numberOfComments)
+        return 1;
+    return 0;
+}
+
+
+function compareByScore(a,b) {
+    if (a.numberOfComments < b.numberOfComments)
+        return -1;
+    if (a.numberOfComments > b.numberOfComments)
+        return 1;
+    return 0;
+}
+
+function convertScoreInNumbers() {
+    mainList.forEach(function(element) {
+        var score;
+        var finalIndex = element.score.indexOf('points');
+        score = element.score.substring(0,finalIndex -1);
+        element.score = Number(score);
+    }.bind(this));
+}
 
 function getEntriesWithMoreThanFiveWordsInTitle() {
     var list = [];
@@ -89,16 +117,24 @@ function getIdsFromComments($,link) {
 
 function getCommentsListIfContainWords($) {
     var list = [];
-    links = $('a'); //jquery get all hyperlinks
+    var numberOfComments = 0;
+    var links = $('a'); //jquery get all hyperlinks
     $(links).each(function(i, link){
         var commentPattern = /^[0-9].+comments$/;
         var comment = $(link).text();
         var matchResult = comment.match(commentPattern);
 
-        if(matchResult || comment === 'discuss' ){
+        if(matchResult){
+            var finalIndex = comment.indexOf('comments');
+            numberOfComments = comment.substring(0,finalIndex -1);
             list.push(
-                {comment:comment, id :getIdsFromComments($,link)}
+                {comments:comment, id :getIdsFromComments($,link), numberOfComments: Number(numberOfComments)}
                 );
+        }
+        else if(comment === 'discuss'){
+            list.push(
+                {comments:comment, id :getIdsFromComments($,link), numberOfComments: numberOfComments}
+            );
         }
     });
     return list;
@@ -112,7 +148,8 @@ function setCommentsinMainList(commentsList) {
         for (var j = 0; j< mainListLenght; j++){
 
             if(mainList[j].id === commentsList[i].id){
-                mainList[j].comment = commentsList[i].comment;
+                mainList[j].comments = commentsList[i].comments;
+                mainList[j].numberOfComments = commentsList[i].numberOfComments;
             }
 
         }
@@ -121,8 +158,8 @@ function setCommentsinMainList(commentsList) {
 
 function countWords(str){
     var count = 0;
-    words = str.split(" ");
-    for (i=0 ; i < words.length ; i++){
+    var words = str.split(" ");
+    for (var i=0 ; i < words.length ; i++){
         // inner loop -- do the count
         if (words[i] != "")
             count += 1;
